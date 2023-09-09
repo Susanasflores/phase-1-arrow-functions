4c23e272964e576aa0164e8fd6791bdd625a7fe4
Susana Solis Flores <Suzy.flores@icloud.com>
2023-09-08 23:10:45 -0400

    arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (parameter1, parameter2) => parameter1 /parameter2;
+divide(2000,100);
+
+const square = root1 => root1 ** 2;
+square(9);
+
+const add = (num1, num2) => num1 + num2;
+add(3,4)
